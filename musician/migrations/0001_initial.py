f325f4082a5480bd16f41203f0d9d3ec73db0c89
= <=>
2025-08-19 19:08:47 +0300

    implement

---
+++ musician/migrations/0001_initial.py
@@ -0,0 +1,24 @@
+import django.core.validators
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Musician',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('first_name', models.CharField(max_length=63)),
+                ('last_name', models.CharField(max_length=63)),
+                ('instrument', models.CharField(max_length=63)),
+                ('age', models.IntegerField(validators=[django.core.validators.MinValueValidator(14, message='We do not accept people under 14')])),
+                ('date_of_applying', models.DateField(auto_now_add=True)),
+            ],
+        ),
+    ]
