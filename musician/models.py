f2fe6cdd36ffbdfd5041c80914ce3d2ac02b289d
= <=>
2025-08-19 18:45:03 +0300

    implement

--- musician/models.py
+++ musician/models.py
@@ -7,8 +7,8 @@ class Musician(models.Model):
     last_name = models.CharField(max_length=63)
     instrument = models.CharField(max_length=63)
     age = models.IntegerField(
-        validators=[MinValueValidator(14,
-        message="We do not accept people under 14")]
+        validators=
+        [MinValueValidator(14, message="We do not accept people under 14")]
     )
     date_of_applying = models.DateField(auto_now_add=True)
 
